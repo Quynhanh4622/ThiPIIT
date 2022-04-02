6e7b4f4600a2cd99d9d4c382f14429623ffac0db
Quynh anh <71160175+Quynhanh4622@users.noreply.github.com>
2022-04-02 09:25:54 +0700

    nopbai

---
+++ Models/Coin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThiPIIT.Models
+{
+    public class Coin
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BaseAsset { get; set; }
+    }
+}
---
+++ Models/Market.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThiPIIT.Models
+{
+    public class Coin
+    {
+    }
+}
